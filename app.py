d84b44fb9f4f0235adfe90fa54829ff5cfc4123a
aaryanrn <itsaaryanagrawal@gmail.com>
2024-11-24 19:22:00 +0530

    Login and Register page addition

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request ,redirect ,session
+from flask_sqlalchemy import SQLAlchemy
+import bcrypt
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' 
+db = SQLAlchemy(app)
+app.secret_key='secret_key'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255),nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+
+    def __init__(self,name,email,password):
+        self.name=name
+        self.email=email
+        self.password=bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self,password):
+        return bcrypt.checkpw(password.encode('utf-8'),self.password.encode('utf-8'))
+    
+# Create the tables when the app starts (Ensure it is in app context)
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        print("in registrer")
+        name=request.form['name']
+        email=request.form['email']
+        password=request.form['password']
+        
+        new_user = User(name=name,email=email,password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/')
+
+    return render_template('index.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+        
+        if user and user.check_password(password):
+            session['email'] = user.email
+            return redirect('/dashboard')
+        else:
+            return render_template('index.html',error='Invalid user')
+
+    return render_template('login.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
